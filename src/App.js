3b1b7aa7fa619993be0e3474681e23480c8efa00
lavanyathangadurai <33346782+lavanyathangadurai@users.noreply.github.com>
2018-03-16 15:43:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Ratingupdate from './component/Ratingupdate';
+import './App.css';
+class App extends Component {
+render() {
+  return (
+      <Ratingupdate />
+        );
+  }
+}
+
+export default App;
---
+++ src/component/Ratingupdate.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import Rating from 'react-star-rating-component';
+    class Ratingupdat extends Component {
+    constructor(props)
+    {
+      super();
+      this.state = {
+        rating : 0
+      };
+    }
+
+    onStarClick(nextValue , prevValue)
+    {
+      this.setState({
+        rating: nextValue
+			});
+			console.log(nextValue);
+    }
+  render() {
+    const { rating } = this.state;
+    return (
+      <div>
+       
+       <div class="container">
+		<div class="card">
+			<div class="container-fliud">
+				<div class="wrapper row">
+					<div class="preview col-md-6">
+						
+						<div class="preview-pic tab-content">
+						  <div class="tab-pane active" id="pic-1"><img src="https://cdn.pixabay.com/photo/2018/03/08/18/27/animals-3209388_1280.jpg" /></div>
+						  <div class="tab-pane" id="pic-2"><img src="http://placekitten.com/400/252" /></div>
+						  <div class="tab-pane" id="pic-3"><img src="http://placekitten.com/400/252" /></div>
+						  <div class="tab-pane" id="pic-4"><img src="http://placekitten.com/400/252" /></div>
+						  <div class="tab-pane" id="pic-5"><img src="http://placekitten.com/400/252" /></div>
+						</div>
+						<ul class="preview-thumbnail nav nav-tabs">
+						  <li class="active"><a data-target="#pic-1" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
+						  <li><a data-target="#pic-2" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
+						  <li><a data-target="#pic-3" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
+						  <li><a data-target="#pic-4" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
+						  <li><a data-target="#pic-5" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a></li>
+						</ul>
+						
+					</div>
+					<div class="details col-md-6">
+						<h3 class="product-title">Domestic Animals</h3>
+						<div class="rating">
+							<div class="stars">
+                            <Rating 
+                           
+                              onStarClick={this.onStarClick.bind(this)}
+                            />
+								{/* <span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span> */}
+							</div>
+							<span class="review-no">41 reviews</span>
+						</div>
+						<p class="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>
+						<h4 class="price">current price: <span>Rs. 45</span></h4>
+						<p class="vote"><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></p>
+						<h5 class="sizes">sizes:
+							<span class="size" data-toggle="tooltip" title="small">s</span>
+							<span class="size" data-toggle="tooltip" title="medium">m</span>
+							<span class="size" data-toggle="tooltip" title="large">l</span>
+							<span class="size" data-toggle="tooltip" title="xtra large">xl</span>
+						</h5>
+						<h5 class="colors">colors:
+							<span class="color orange not-available" data-toggle="tooltip" title="Not In store"></span>
+							<span class="color green"></span>
+							<span class="color blue"></span>
+						</h5>
+						<div class="action">
+							<button class="add-to-cart btn btn-default" type="button">add to cart</button>
+							<button class="like btn btn-default" type="button"><span class="fa fa-heart"></span></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    
+      </div>
+      
+    );
+  }
+}
+
+export default Ratingupdat;
